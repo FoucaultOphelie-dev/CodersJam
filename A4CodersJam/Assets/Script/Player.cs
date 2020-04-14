31de10c3871ed207f02911434f75c2db1159422c
PAG <pierregirompaire@gmail.com>
2020-04-14 13:08:32 +0200

    Sprite mask + score + movement

---
+++ A4CodersJam/Assets/Script/Player.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoderJam
+{
+    public class Player : MonoBehaviour
+    {
+        public float moveSpeed = 5f;
+        public GameObject spawn;
+        public GameObject Haut;
+        public GameObject Bas;
+        public GameObject Gauche;
+        public GameObject Droite;
+        public GameObject tache;
+
+        public static int life = 3;
+        public static int death = 0;
+
+        private Rigidbody rb;
+        private Vector3 movement;
+
+        private void Awake()
+        {
+
+            life = 3;
+            death = 0;
+            rb = this.GetComponent<Rigidbody>();
+
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float hAxis = Input.GetAxis("Horizontal");
+            float vAxis = Input.GetAxis("Vertical");
+            movement = new Vector3(hAxis, vAxis, 0) * moveSpeed * Time.deltaTime;
+
+            explosion();
+        }
+
+        private void FixedUpdate()
+        {
+            rb.MovePosition(transform.position + movement);
+        }
+
+        void explosion()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(tache, Haut.transform.position, Quaternion.identity);
+                Instantiate(tache, Bas.transform.position, Quaternion.identity);
+                Instantiate(tache, Gauche.transform.position, Quaternion.identity);
+                Instantiate(tache, Droite.transform.position, Quaternion.identity);
+                life--;
+                lifeManager();
+            }
+        }
+
+        void lifeManager()
+        {
+            if (life <= 0)
+            {
+                this.transform.position = spawn.transform.position;
+                death++;
+                life = 3;
+            }
+        }
+    }
+}
---
+++ A4CodersJam/Assets/Script/ScoreManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CoderJam
+{
+    public class ScoreManager : MonoBehaviour
+    {
+        public Text textLife;
+        public Text textDeath;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            textLife.text = "Life :"+ Player.life;
+            textDeath.text = "Death :" + Player.death;
+        }
+    }
+}
